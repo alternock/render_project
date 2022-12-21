a43e6788edee9978b279b84857fccafd8b44ce01
Juan Carlos <montilla269@gmail.com>
2022-12-21 19:20:57 -0300

    add render_template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Hello World!"})
+    return render_template("index.html")
 
 @app.route("/users")
 def users():
